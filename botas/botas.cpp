819f4a63c9de539a7e08577c82cac5a49f355156
serramatutu <lucasvvop@hotmail.com>
2018-03-21 11:44:04 -0300

    Botas sem overflow

--- botas/botas.cpp
+++ botas/botas.cpp
@@ -5,37 +5,34 @@ https://olimpiada.ic.unicamp.br/pratique/p2/2017/f1/botas/
 #include <iostream>
 #include <string>
 #include <map>
+#include <algorithm>
 
 using namespace std;
+
+struct bota {
+    int dir = 0;
+    int esq = 0;
+};
 
 int main() {
     int count;
     cin >> count;
 
-    map<int, int> botas;
+    map<int, bota> botas;
     for (int i=0; i<count; i++) {
         char lado;
-        int bota;
-        cin >> bota >> lado;
-     
-        // Coloca a bota no dicionário caso ela não exista
-        if (botas.count(bota) <= 0)
-            botas[bota] = 1;
+        int tamanho;
+        cin >> tamanho >> lado;
 
         if (lado == 'D')
-            botas[bota] *= 2;
+            botas[tamanho].dir += 1;
         else
-            botas[bota] *= 3;
+            botas[tamanho].esq += 1;
     }
 
     int validPairs = 0;
-    for (auto it = botas.begin(); it != botas.cend(); it++) {
-        int value = it->second;
-        while (value % 6 == 0 && value != 0) { // Se tem ambos os pés
-            value %= 6;
-            validPairs++;
-        }
-    }
+    for (auto it = botas.cbegin(); it != botas.cend(); it++)
+        validPairs += min(it->second.esq, it->second.dir);
 
     cout << validPairs;
 
---
+++ o-chefe/ochefe.cpp
@@ -0,0 +1,18 @@
+/* 
+Link para o problema:
+https://olimpiada.ic.unicamp.br/pratique/p2/2017/f1/chefe/
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Grafo {
+    
+}
+
+int main() {
+
+
+    return 0;
+}
