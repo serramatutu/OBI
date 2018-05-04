fd83474bd5356ff918e96da44f208fe46411079e
serramatutu <lucasvvop@hotmail.com>
2018-05-03 22:27:18 -0300

    cortando-papel em cpp

--- cortando-papel/solution.cpp
+++ cortando-papel/solution.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 using namespace std;
 
-vector<int> simplify(const vector<int>& heights) {
+int getSign(int a) {
+    if (a < 0) return -1;
+    if (a > 0) return 1;
+    return 0;
+}
 
+vector<int> simplify(const vector<int>& heights) {
+    int minHeight = INT32_MAX;
+    for (auto it = heights.cbegin(); it != heights.cend(); it++)
+        if (*it < minHeight)
+            minHeight = *it;
+    minHeight--;
+
+    int sign = getSign(heights[1] - heights[0]);
+    vector<int> newHeights(1, heights[0] - minHeight);
+
+    // remove colunas iguals, escadas e colunas desnecessárias no geral
+    int lastSign;
+    for (int i=1; i < heights.size() - 1; i++) {
+        lastSign = sign;
+        sign = heights[i + 1] - heights[i];
+
+        if (lastSign != sign && lastSign != 0)
+            newHeights.push_back(heights[i] - minHeight);
+    }
+
+    return newHeights;
+}
+
+int solve(const vector<int>& heights) {
+    
 }
 
 int main() {
@@ -13,17 +42,14 @@ int main() {
     int nHeights;
     cin >> nHeights;
 
-    vector<int> heights(nHeights);
-
     // Lê as alturas
-    int height;
-    for (int i=0; i<nHeights; i++) {
-        cin >> height;
-        heights.push_back(height);
-    }
+    vector<int> heights(nHeights);
+    for (int i=0; i<nHeights; i++)
+        cin >> heights[i];
+    
+    // Simplifica o histograma sem que perca suas propriedades
+    heights = simplify(heights);
 
-    for(auto it = heights.cbegin(); it != heights.cend(); it++)
-        cout << *it;
-
+    cout << solve(heights);
     return 0;
 }
