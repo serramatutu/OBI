f09915e2ec1cab993a3bc3d083a3db0af1bd9bff
serramatutu <lucasvvop@hotmail.com>
2018-05-05 17:49:17 -0300

    Um pouco melhor

--- cortando-papel/solution.cpp
+++ cortando-papel/solution.cpp
@@ -18,11 +18,15 @@ vector<int> simplify(const vector<int>& heights) {
             minHeight = *it;
     minHeight--;
 
-    int sign = getSign(heights[1] - heights[0]);
-    vector<int> newHeights(1, heights[0] - minHeight);
+    vector<int> newHeights;
+    newHeights.reserve(heights.size());
+
+    if (heights[0] > heights[1]) // o primeiro só é inserido se for pico
+        newHeights.push_back(heights[0] - minHeight);
 
     // remove colunas iguals, escadas e colunas desnecessárias no geral
     int lastSign;
+    int sign = getSign(heights[1] - heights[0]);
     for (int i=1; i < heights.size() - 1; i++) {
         lastSign = sign;
         sign = getSign(heights[i + 1] - heights[i]);
@@ -31,29 +35,29 @@ vector<int> simplify(const vector<int>& heights) {
             newHeights.push_back(heights[i] - minHeight);
     }
 
-    // O último deve ser sempre parte do vetor
-    newHeights.push_back(heights[heights.size() - 1] - minHeight);
+    if (heights[heights.size() - 1] > heights[heights.size() - 2]) // último só é inserido se for pico
+        newHeights.push_back(heights[heights.size() - 1] - minHeight);
 
     return newHeights;
 }
 
-
+// Supõe que esteja simplificado
 int solve(const vector<int>& heights) {
     unordered_multimap<int, int> heightMap;
 
-    int valley = 0; // vales estão nos pares
-    if (heights[1] - heights[0] < 0 ) // se o segundo é maior que o primeiro
-        valley = 1; // vales estão nas casas ímpares
+    int valley = 0; // vales estão nas colunas pares
+    if (heights[1] < heights[0]) // se o segundo é menor que o primeiro
+        valley = 1; // vales estão nas colunas ímpares
 
     int maxHeight = 0;
     for (int i=0; i<heights.size(); i++) {
         heightMap.insert({heights[i], i}); // Mapeia a posição atual à altura atual
         
-        if (maxHeight < heights[i]) // encontra a maior altura
+        if (maxHeight <= heights[i]) // encontra a maior altura
             maxHeight = heights[i];
     }
 
-    int maxPieces = 2,
+    int maxPieces = 1, 
         pieces = 1; // corte fora do papel resulta em 1 pedaço
     // vai descendo as alturas
     for (int currentHeight = maxHeight; currentHeight > 0; currentHeight--) {
@@ -61,9 +65,9 @@ int solve(const vector<int>& heights) {
         for (auto columnInterator = cols.first; columnInterator != cols.second; columnInterator++) {
             int currentColumnIndex = columnInterator->second;
             if (currentColumnIndex % 2 - valley == 0) // se a coluna atual é um vale
-                pieces--;
+                pieces--; // fecha um corte
             else
-                pieces++;
+                pieces++; // se não é um vale, o corte atual é dividido em dois
         }
 
         if (pieces > maxPieces)
@@ -85,6 +89,10 @@ int main() {
     
     // Simplifica o histograma sem que perca suas propriedades
     heights = simplify(heights);
+
+    for(auto it=heights.cbegin(); it != heights.cend(); it++)
+        cout << *it << " ";
+    cout << endl;
 
     cout << solve(heights);
     return 0;
