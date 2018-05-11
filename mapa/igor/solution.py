a6a5384ef5afca36e8fa866736e41f42d3a98510
Igormandello <igormandello@gmail.com>
2018-05-11 08:21:17 -0300

    Melhorada solução de mapa
    - Agora não da mais timeout

--- mapa/igor/solution.py
+++ mapa/igor/solution.py
@@ -8,16 +8,13 @@ def main():
     # y: init[0]  x: init[1]
     init = -1
     for _ in range(l):
-        m.append(input().strip())
+        m.append(list(input()))
 
         if init == -1:
             for i in range(c):
                 if m[-1][i] == 'o':
                     init = [len(m) - 1, i]
                     break
-        
-    visited = []
-    visited.append(init)
 
     toVisit = []
     toVisit.append(init)
@@ -28,29 +25,31 @@ def main():
 
         if actual[0] != l - 1:
             nextPos = [actual[0] + 1, actual[1]]
-            if m[actual[0] + 1][actual[1]] == 'H' and nextPos not in visited:
-                visited.append(nextPos)
+            if m[actual[0] + 1][actual[1]] == 'H':
+                m[actual[0]][actual[1]] = '.'
                 toVisit.append(nextPos)
 
         if actual[0] != 0:
             nextPos = [actual[0] - 1, actual[1]]
-            if m[actual[0] - 1][actual[1]] == 'H' and nextPos not in visited:
-                visited.append(nextPos)
+            if m[actual[0] - 1][actual[1]] == 'H':
+                m[actual[0]][actual[1]] = '.'
                 toVisit.append(nextPos)
 
         if actual[1] != c - 1:
             nextPos = [actual[0], actual[1] + 1]
-            if m[actual[0]][actual[1] + 1] == 'H' and nextPos not in visited:
-                visited.append(nextPos)
+            if m[actual[0]][actual[1] + 1] == 'H':
+                m[actual[0]][actual[1]] = '.'
                 toVisit.append(nextPos)
                 
         if actual[1] != 0:
             nextPos = [actual[0], actual[1] - 1]
-            if m[actual[0]][actual[1] - 1] == 'H' and nextPos not in visited:
-                visited.append(nextPos)
+            if m[actual[0]][actual[1] - 1] == 'H':
+                m[actual[0]][actual[1]] = '.'
                 toVisit.append(nextPos)
 
     print(str(actual[0] + 1) + " " + str(actual[1] + 1))
+
+
 
 if __name__ == '__main__':
     main()
