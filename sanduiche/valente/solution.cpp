14511a20baf86045f1bcfaa3d6ef6277b3fb3911
serramatutu <lucasvvop@hotmail.com>
2018-05-17 00:02:07 -0300

    sanduiche em cpp quase lá

---
+++ sanduiche/valente/solution.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+int n, d;
+
+int min = 0;
+int max = 0;
+long int sum;
+int qtd = 0;
+
+bool minRepeat = false;
+bool maxRepeat = false;
+
+std::vector<int> pieces;
+
+void increaseMax() {
+    max++;
+    if (max / n >= 1) {
+        max = 0;
+        maxRepeat = true;
+    }
+    sum += pieces[max];
+}
+
+void increaseMin() {
+    sum -= pieces[min];
+    min++;
+    if (min / n >= 1) {
+        min = 0;
+        minRepeat = true;
+    }
+}
+
+
+int main() {
+    std::cin >> n >> d;
+
+    pieces.reserve(n);
+
+    for (int i=0; i<n; i++) {
+        int current;
+        std::cin >> current;
+
+        pieces.push_back(current);
+    }
+
+    sum = pieces[0];
+    
+    while (!(maxRepeat && minRepeat)) {
+        if (sum < d) {
+            increaseMax();
+        }
+        else if (sum > d) {
+            increaseMin();
+        }
+        else { // sum == d
+            qtd++;
+            increaseMin();
+        }
+    }
+
+    std::cout << qtd;
+
+    return 0;
+}
