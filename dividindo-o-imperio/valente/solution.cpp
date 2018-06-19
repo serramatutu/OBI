d594ee03a7a33859c6a44d7db984e2a509363edb
serramatutu <lucasvvop@hotmail.com>
2018-06-18 23:42:26 -0300

    dividindo o império quase funcionando

---
+++ dividindo-o-imperio/valente/solution.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int N, minDiff;
+vector<unordered_set<int>> graph;
+
+unordered_set<int> visited;
+int dfs(int from, int to) {
+    int count = 1; // quantos nós abaixo existem (inclusive)
+
+    visited.insert(from);
+
+    for (int neighbor : graph[from]) {
+        if (visited.count(neighbor) <= 0) {
+            count += dfs(neighbor, from);
+        }
+    }
+
+    int diff = abs(N - 2*count); // (N - count) - count
+    if (diff < minDiff)
+        minDiff = diff;
+
+    return count;
+}
+
+int main() {
+    cin >> N;
+    minDiff = N;
+    graph.resize(N);
+
+    for (int i=0; i<N; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        graph[a].insert(b);
+        graph[b].insert(a);
+    }
+
+    dfs(0, -1);
+    cout << minDiff << endl;
+
+    return 0;
+}
